406671b4dd27d282940b3819df85a2b4e5de5794
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-05-15 21:20:05 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Product } from './Product';
+
+
+const product =  new Product();
+
+console.log('Product');
---
+++ tests/Product.ts
@@ -0,0 +1,23 @@
+import { Product } from '../src/Product';
+
+describe('Product', () => {
+  let product: Product;
+
+  beforeEach(() => {
+    product = new Product();
+
+
+  });
+
+  it('should check ...', () => {
+    // Test case for ...
+    // Arrange
+    // Act
+    // Assert
+
+  });
+
+  // Add more test cases for other methods of Product class
+
+
+});
